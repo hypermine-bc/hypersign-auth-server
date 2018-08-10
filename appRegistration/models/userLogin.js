fea64c6a501151ca419a680be2c383d02a872439
vishwas.bhushan@imaginea.com <vishwasanand.cse@gmail.com>
2018-08-11 00:35:31 +0530

    verify signedMsg implemented. yet to be tested

--- appRegistration/models/userLogin.js
+++ appRegistration/models/userLogin.js
@@ -1,5 +1,7 @@
+import hsWallet from '../common/hypersign-wallet'
 const User = require('../schema/user')
 var Pusher = require('pusher');
+
 
 var pusher = new Pusher({
   appId: '570580',
@@ -13,8 +15,28 @@ const userAuth = (data) => {
     return User.findOne({publicToken: data.attributes.publicToken, companyId: data.attributes.companyId})
         .then((currentUser) => {
             if (currentUser) {
-            	pusher.trigger('hypermine-hypersign', 'auth-service', currentUser)
-                return {"data":currentUser,"message":"Valid User"}
+                let signedMsgRSV =  data.attributes.signedRsv
+                let from = data.attributes.publicToken
+                let rawMsg = data.attributes.rawMsg
+                //verify signedMsg and get the public key
+                // check if newPublicKey is equal to oldPublicKey
+                let verifyPromise = hsWallet.verifyMessageTx(rawMsg, signedMsgRSV, from)
+                verifyPromise.then((res) =>{
+                    if(res){
+                        console.log('userLogin : userAuth : Promise resolved. Successfully verfied.')    
+                        pusher.trigger('hypermine-hypersign', 'auth-service', currentUser)
+                        return {"data":currentUser,"message":"Valid User"}
+                    }
+                    else{
+                        console.log('userLogin : userAuth : Promise resolved. Unverifed.')        
+                        pusher.trigger('hypermine-hypersign', 'auth-service', currentUser)
+                        return {"data":{},"message":"Invalid User"}
+                    } 
+                  } , (err) => {
+                    console.log('userLogin : userAuth : Error Promise not resolved. Message = '+ err)
+                }).catch((err) => {
+                    console.log('userLogin : userAuth : Error in verifyPromise. Message = '+ err)
+                })
             } else {
                 return {"data":{},"message":"Invalid User"}
             }

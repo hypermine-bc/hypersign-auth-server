ff50a6bf7283ec93a9d42a23e2553cb21f47fca5
root <vikram@nanocorporation.co.uk>
2018-08-10 16:28:18 +0530

    Added pusher support

--- appRegistration/models/userLogin.js
+++ appRegistration/models/userLogin.js
@@ -5,6 +5,9 @@ const userAuth = (data) => {
     return User.findOne({publicToken: data.attributes.publicToken, companyId: data.attributes.companyId})
         .then((currentUser) => {
             if (currentUser) {
+
+            	//trigger it when the signedTraction is validated 
+            	pusher.trigger('hypermine-hypersign', 'auth-service', currentUser)
                 return { response: 'Valid User' }
             } else {
                 return { response: 'InValid User' }

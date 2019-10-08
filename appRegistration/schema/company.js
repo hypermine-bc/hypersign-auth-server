ac38cc09973bd6b4f5cb93a6f39e76915fd861bf
vishwas1 <vishwasanand.cse@gmail.com>
2019-10-08 18:40:32 +0530

    changed company type to string

--- appRegistration/schema/company.js
+++ appRegistration/schema/company.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const companySchema = new Schema({
-    companyId: { type: Number, required: true },
+    companyId: { type: String, required: true },
     companyName: { type: String, required: true },
     publicToken: { type: String, required: true },
     other: { type: String, required: false },
